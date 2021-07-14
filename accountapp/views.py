20cc0574dc212872c9586347686f3d0e6de49b99
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-07-14 14:55:02 +0900

    7/14_create_view

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -24,3 +27,13 @@ def hello_world(request):
 
         return render(request,'accountapp/hello_world.html',
                       context={"data_list" : data_list})
+
+
+
+
+class AccountCreateView(CreateView):
+    # 회원가입 로직 구현!
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # 나중에 내용을 과
+    template_name = 'accountapp/create.html'
