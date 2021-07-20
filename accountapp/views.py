a1e111d287272d4da69ac7f0b5c3a25d1b35addb
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-07-20 10:25:46 +0900

    7/20_delete_info

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.models import NewModel
 
@@ -50,3 +50,9 @@ class AccountUpdateView(UpdateView):
     context_object_name = "target_user"  # 객체에 접근할 때 이름 설정
     success_url = reverse_lazy("accountapp:hello_world"),  # 어디로 재연결할 것인가
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
