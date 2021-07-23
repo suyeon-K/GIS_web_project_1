933340797c81e8a852f59e64c863116c464c7f21
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-07-23 10:20:14 +0900

    7/23_make_profileapp

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp', # 이 앱을 사용하겠다!
-    'bootstrap4'
+    'bootstrap4',
+    'profileapp'
 ]
 
 MIDDLEWARE = [
