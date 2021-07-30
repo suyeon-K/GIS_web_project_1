ddaa2b51448f3dcff086b527ea819f76cfbc6c9b
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-07-30 10:24:05 +0900

    7/30_create_article_app

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accountapp', # 이 앱을 사용하겠다!
     'bootstrap4',
-    'profileapp'
+    'profileapp',
+    'articleapp'
 ]
 
 MIDDLEWARE = [
