a259136584ec338ac4f30f266d2d2f55f451d224
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-08-18 15:39:57 +0900

    8/18_connet_project_and_articles

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -9,3 +10,6 @@ class Project(models.Model):
     image = models.ImageField(upload_to='project/', null=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
