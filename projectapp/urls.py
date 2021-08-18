a9dd7e327075aeb3e15edb7b6ccdf1bf84c14c33
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-08-18 14:57:13 +0900

    8/18_project_list

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'projectapp'
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+
     path('list/', ProjectListView.as_view(), name='list'),
 ]
