cd63ad03d5af1506067bfef4c5815ce86209cae5
suyeon-K <74526895+suyeon-K@users.noreply.github.com>
2021-08-27 09:16:12 +0900

    8/27_remove_hello_world

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,34 +16,35 @@ from accountapp.models import NewModel
 from articleapp.models import Article
 
 
-@login_required
-def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-
-            temp = request.POST.get('input_text')
-
-            model_instance = NewModel()
-            model_instance.text = temp
-            model_instance.save()
-
-            return HttpResponseRedirect(reverse("accountapp:hello_world"))
-
-        else:
-
-            data_list = NewModel.objects.all()
-
-            return render(request,'accountapp/hello_world.html',
-                          context={"data_list" : data_list})
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
-
+# @login_required
+# def hello_world(request):
+#     if request.user.is_authenticated:
+#         if request.method == "POST":
+#
+#             temp = request.POST.get('input_text')
+#
+#             model_instance = NewModel()
+#             model_instance.text = temp
+#             model_instance.save()
+#
+#             return HttpResponseRedirect(reverse("accountapp:hello_world"))
+#
+#         else:
+#
+#             data_list = NewModel.objects.all()
+#
+#             return render(request,'accountapp/hello_world.html',
+#                           context={"data_list" : data_list})
+#     else:
+#         return HttpResponseRedirect(reverse('accountapp:login'))
+#
 
 class AccountCreateView(CreateView):
     # 회원가입 로직 구현!
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world') # 나중에 내용을 과
+    # success_url = reverse_lazy('accountapp:hello_world') # 나중에 내용을 과
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -79,6 +80,6 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
